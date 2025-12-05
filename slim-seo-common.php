301d6408a6c15ae6a74a7225f1a56f9b4477b355
Anh Tran <anhtnt@elightup.com>
2025-12-05 16:09:32 +0700

    Add meta files

---
+++ slim-seo-common.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Slim SEO Common
+ * Description: Common code for Slim SEO plugins
+ * Author:      Slim SEO
+ * Author URI:  https://wpslimseo.com
+ * License:     GPL v3
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
